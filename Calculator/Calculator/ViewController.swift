a56d0a4750d8292473e89602c119b9a59e14590c
Stephanie Zablocki <stephaniezablocki@gmail.com>
2021-10-15 15:06:50 -0400

    Fixed file structure

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Stephanie Zablocki on 10/13/21.
+//  Copyright © 2021 Stephanie Zablocki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+
+}
+
