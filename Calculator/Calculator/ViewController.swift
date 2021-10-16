d859c75200cae4debc5a5b79998d493e59dbf92a
Stephanie Zablocki <stephaniezablocki@gmail.com>
2021-10-15 20:18:19 -0400

    Added pi and sqrt functionality

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,10 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
     @IBAction func touchDigit(_ sender: UIButton) {
-        print("Touch digit was called");
-        
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        }
+        else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
     }
     
+    var displayValue : Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                display.text = String(Double.pi)
+            case "√":
+                let operand = Double(display.text!)!
+                display.text = String(sqrt(operand))
+            default:
+                break
+            }
+        }
+    }
 }
 
